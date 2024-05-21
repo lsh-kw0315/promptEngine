c1e89bcd92725d63860faeb39956807b16c5c2d0
Bmethyst <48680923+Bmethyst@users.noreply.github.com>
2024-05-21 18:43:18 +0900

    하이퍼파라미터 작업
    query의 prefix 형태로 임시 작업했으나, 반복해서 같은 질문을 넣어서 답변 기복이 매우 심함.

--- restApi/urls.py
+++ restApi/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = 'restApi'
 urlpatterns = [
     path('llama/', views.llama, name='request-llama'),
+    path('llama2/', views.llama2, name='request-llama2'),
     path('', views.prompt_test_page, name='prompt_test_page'),
     path('generator/<str:query>', views.prompt_generator, name='request_generator')
 ]
--- restApi/views.py
+++ restApi/views.py
@@ -18,8 +18,10 @@ client = OpenAI(base_url="http://localhost:8000/v1", api_key="None")
 chat_log = deque()
 log_maxlen = 3
 
-model = BartForConditionalGeneration.from_pretrained("restApi/model/chatgpt-prompt-generator", from_tf=True)
-tokenizer = BartTokenizer.from_pretrained("restApi/model/chatgpt-prompt-generator")
+model = BartForConditionalGeneration.from_pretrained("merve/chatgpt-prompt-generator-v12", from_tf=True)
+#model = BartForConditionalGeneration.from_pretrained("meta-llama/Meta-Llama-3-8B-Instruct", from_tf=True)
+tokenizer = BartTokenizer.from_pretrained("merve/chatgpt-prompt-generator-v12")
+#tokenizer = BartTokenizer.from_pretrained("meta-llama/Meta-Llama-3-8B-Instruct")
 device = "cuda" if torch.cuda.is_available() else "cpu"
 model.to(device)
 
@@ -44,6 +46,20 @@ def llama(request):
     return JsonResponse(data)
 
 
+def llama2(request):
+    query = request.POST['query']
+    data = {}
+    answer = chat2(query)
+    data['query'] = query
+    data['answer'] = answer
+    data['chatLog'] = ''.join(chat_log)
+
+    serializer = RestApiSerializer(data=data)
+    if serializer.is_valid():
+        serializer.save()
+    return JsonResponse(data)
+
+
 def chat(query):
     chatting = "\n".join(chat_log)
     current_chat = f"\nQ : {query}.\nA : "
@@ -58,6 +74,24 @@ def chat(query):
     '''deque.append(query + ' ' + answer)
     if len(deque) > 3:
         deque.popleft()'''
+    return answer
+
+
+def chat2(query):
+    chatting = "\n".join(chat_log)
+    current_chat = f'The task for you is to rephrase user queries into hyperparameter task prompts that make it easier for other text generation AI to understand the requirements. For example, if I ask: \n\n"I\'m planning a trip to Japan next month. Can you provide some recommendations for must-see attractions and activities in Tokyo?",\n\n you should rephrase it as: \n\n"Provide recommendations on top attractions, activities, and experiences in Tokyo suitable for a one-month trip. Include information on convenient public transportation options, tips for efficient navigation, and any other helpful advice for first-time visitors. [Task: Travel Recommendations] [Destination: Tokyo, Japan] [Duration: 1 month] [Requirements: Top Attractions, Activities, Transportation, Travel Tips]"\n\nYour role is not(never) to *answer* the questions I ask but to rephrase them into *clear prompts with hyperparameters*(must) that make it easier for the text generation AI to understand and provide relevant responses. Now, here\'s my real question. \n Q: "{query}"'
+    chatting += current_chat
+    output = client.completions.create(
+        model="Meta-Llama-3-8B-Instruct-Q4_K_M.gguf",
+        prompt=chatting,
+        max_tokens=2048, stop=["Q:", "\n"],
+    )
+    print(chatting)
+    answer = str(output.choices[0].text)
+    '''deque.append(query + ' ' + answer)
+    if len(deque) > 3:
+        deque.popleft()'''
+    print(answer)
     return answer
 
 
