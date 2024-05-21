3d57dc2ba292769e251f573c03f3c1728b2efa2a
LSH-kw0315 <tmdgjsdl1234@naver.com>
2024-05-21 20:32:10 +0900

    geval only english

--- restApi/urls.py
+++ restApi/urls.py
@@ -5,5 +5,6 @@ app_name = 'restApi'
 urlpatterns = [
     path('llama/', views.llama, name='request-llama'),
     path('', views.prompt_test_page, name='prompt_test_page'),
-    path('generator/<str:query>', views.prompt_generator, name='request_generator')
+    path('generator/<str:query>', views.prompt_generator, name='request_generator'),
+    path('geval/',views.geval,name='request-geval')
 ]
